23f238deb594fb49a17f27482c0ab2e3006023b5
Ryan Daly <S00237889@atu.ie>
2024-05-13 15:15:53 +0100

    wpf app ui done

---
+++ Migrations/test1.cs
@@ -0,0 +1,44 @@
+﻿namespace FinalExam.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class Test2 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Bookings",
+                c => new
+                {
+                    BookingID = c.Int(nullable: false, identity: true),
+                    BookingsDate = c.DateTime(nullable: false),
+                    NumberOfParticipants = c.Int(nullable: false),
+                    CustomerID = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.BookingID)
+                .ForeignKey("dbo.Movies", t => t.CustomerID, cascadeDelete: true)
+                .Index(t => t.CustomerID);
+
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                {
+                    CustomerID = c.Int(nullable: false, identity: true),
+                    CustomerName = c.String(nullable: false),
+                    CustomerNumber = c.Int(nullable: false),
+                })  
+
+                .PrimaryKey(t => t.CustomerID);
+
+        }
+
+        public override void Down()
+        {
+            DropForeignKey("dbo.Bookings", "CustomeID", "dbo.Customers");
+            DropIndex("dbo.Bookings", new[] { "CustomerID" });
+            DropTable("dbo.Customers");
+            DropTable("dbo.Bookings");
+        }
+    }
+}
